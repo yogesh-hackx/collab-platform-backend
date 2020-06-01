885fa93b42ed91d97a8fdacd854c853b1d23296d
Ashutosh619-sudo <Ashutoshsinghrkt@gmail.com>
2020-06-02 01:55:33 +0530

    add get uder by id

--- routes/user.js
+++ routes/user.js
@@ -8,4 +8,16 @@ router.get('/', async (req, res) => {
     res.json(users);
 });
 
+
+router.get('/:id',async (req,res)=>{
+    await User.findById({ _id: req.params.id })
+        .then((user) => {
+            if (!user) {
+                return res.status(404).end();
+            }
+            return res.json(user);
+        })
+        .catch((err) => console.log(err));
+});
+
 module.exports = router;
