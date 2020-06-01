2b20a09b5bb238250fb5f8c892712500b4ed5b81
yogesh-hackx <yogesh.hackx@gmail.com>
2020-06-02 00:53:27 +0530

    added eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    env: {
+        commonjs: true,
+        es6: true,
+        node: true,
+    },
+    extends: ['airbnb-base'],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaVersion: 11,
+    },
+    rules: {
+        indent: ['error', 4],
+    },
+};
